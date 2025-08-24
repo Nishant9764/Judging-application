dafac5197ad10f1568c8eb5377e00112570b3d48
NitheshNaik <nitheshbnaik@gmail.com>
2025-08-24 18:47:40 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+let port = 8080;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/home", (req, res) => {
+    res.render("home.ejs");
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});
